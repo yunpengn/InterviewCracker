42c5b092fbce58e5ef96e820f67d6bbcd0451bc3
Yunpeng Niu <neilniuyunpeng@gmail.com>
2017-12-12 22:40:14 +0800

    Add code to first problem

--- src/Chap01/IsUnique.java
+++ src/Chap01/IsUnique.java
@@ -1,5 +1,45 @@
 package Chap01;
 
-public class IsUnique {
+import java.util.Arrays;
+import java.util.Collections;
 
+public class IsUnique {
+    public static void main(String[] args) {
+        IsUnique checker = new IsUnique();
+        System.out.println(checker.ifAllAscii("Java!"));
+        System.out.println(checker.ifAllAscii("Ruby!"));
+
+        System.out.println(checker.sortThem("Java!"));
+        System.out.println(checker.sortThem("Ruby!"));
+    }
+
+	public boolean ifAllAscii(String str) {
+		boolean[] flags = new boolean[128];
+        char[] characters = str.toCharArray();
+
+        for (char c : characters) {
+            int charCode = (int) c;
+
+            if (flags[charCode]) {
+                return false;
+            }
+
+            flags[charCode] = true;
+        }
+
+        return true;
+	}
+
+	public boolean sortThem(String str) {
+        char[] characters = str.toCharArray();
+        Arrays.sort(characters);
+
+        for (int i = 0; i < str.length() - 1; i++) {
+            if (characters[i] == characters[i + 1]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
